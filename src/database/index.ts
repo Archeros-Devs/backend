3526d89bac3dd72e2767f8872955c416cb71ef47
Nicholas Ferreira <nichoas_sc@live.com>
2020-09-25 00:32:59 -0300

    database rds

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,25 @@ import { createConnection, Connection } from "typeorm";
 
 class Database {
   async MySQL(database = 'default'): Promise<Connection> {
-    return createConnection(database)
+    return createConnection({
+      type: "mysql",
+      host: process.env.RDS_HOSTNAME || "localhost",
+      username: process.env.RDS_USERNAME || "root",
+      password: process.env.RDS_PASSWORD || "",
+      port: parseInt(process.env.RDS_PORT) || 3306,
+      database: "peruibemelhor",
+      synchronize: false,
+      logging: !process.env.RDS_HOSTNAME,
+      entities: [
+        "src/entity/**/*.ts"
+      ],
+      migrations: [
+        "src/database/migration/**/*.ts"
+      ],
+      subscribers: [
+        "src/subscriber/**/*.ts"
+      ]
+    });
   }
 }
 
